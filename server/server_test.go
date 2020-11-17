942153daab8ec09c5d91d76a7c6e29a95ec5da85
var-kyle <kyle.meelker@gmail.com>
2020-11-17 16:36:35 -0500

    adding unit tests for /healthcheck and server

---
+++ server/server_test.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/DTS-STN/benefit-service/handlers"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type HandlerServiceMock struct {
+	mock.Mock
+}
+
+func (m *HandlerServiceMock) HealthCheck(c echo.Context) error {
+	args := m.Called()
+	return args.Error(1)
+}
+
+// TODO: This doesn't work, need to setup an http client and call the endpoints to run tests
+func TestServer(t *testing.T) {
+	e := echo.New()
+
+	// Create a Mock for the interface
+	handlerMock := new(HandlerServiceMock)
+	// Add a mock call request
+	handlerMock.On("HealthCheck", e).
+		Return(nil)
+	// Set the mock to be used by the code
+	handlers.HandlerService = handlers.HandlerServiceInterface(handlerMock)
+
+	// TODO: Add Tests
+
+}
