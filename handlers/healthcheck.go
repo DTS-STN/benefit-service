f2fddeca26b66af1d56a84c37661ee7c7d7b9932
var-kyle <kyle.meelker@gmail.com>
2020-11-26 10:51:24 -0500

    much change, such wow

--- handlers/healthcheck.go
+++ handlers/healthcheck.go
@@ -13,5 +13,5 @@ import (
 // @Success 200 {string} string	"Healthy"
 // @Router /healthcheck [get]
 func (h *Handler) HealthCheck(c echo.Context) error {
-	return c.String(http.StatusOK, "Hethy")
+	return c.String(http.StatusOK, "Heeley")
 }
