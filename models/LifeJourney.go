65f522e3d61bfbbab5d65b375b86f562e427ae38
MikeBungay <mikebungay76@gmail.com>
2020-11-26 16:00:52 -0500

    Fixed Formating issue

--- models/LifeJourney.go
+++ models/LifeJourney.go
@@ -4,8 +4,8 @@ type LifeJourney struct {
 	ID              int            `json:"id"`
 	Title           string         `json:"title"`
 	Description     string         `json:"description"`
-	TitleFr        string         `json:"title_fr"`
-	DescriptionFr  string         `json:"description_fr"`
+	TitleFr         string         `json:"title_fr"`
+	DescriptionFr   string         `json:"description_fr"`
 	RelatedBenefits []string       `json:"related_benefits"`
 	BenefitDetails  []FieldDetails `json:"lifejourney_details"`
 }
