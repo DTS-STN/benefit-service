a2c2385fdecac05c3e40c5d7e519204e662b33f2
MikeBungay <mikebungay76@gmail.com>
2020-11-30 09:17:31 -0500

    Updating the Go Test to fix the path to the data file for the test

--- handlers/life_journey_test.go
+++ handlers/life_journey_test.go
@@ -13,14 +13,14 @@ func TestLifeJourney(t *testing.T) {
 	// Setup Echo service
 	e := echo.New()
 
-	lifejourneys.LifeJourneyService = lifejourneys.LifeJourneyServiceStruct{Filename: "../life_journeys.json"}
+	lifejourneys.LifeJourneyService = lifejourneys.LifeJourneyServiceStruct{Filename: "life_journeys.json"}
 	// Setup http request using httptest
 	req := httptest.NewRequest(http.MethodGet, "/lifejourney", nil)
 	// Create a httptest record
 	rec := httptest.NewRecorder()
 	// Create a new Echo Context
 	c := e.NewContext(req, rec)
-	
+
 	// Assertions
 	if assert.NoError(t, HandlerService.LifeJourney(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
