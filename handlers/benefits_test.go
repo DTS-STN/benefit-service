54de8130bd6bc81b2ee76d0c9623720f271e0611
var-kyle <kyle.meelker@gmail.com>
2020-12-08 13:28:55 -0500

    fixing unit tests

--- handlers/benefits_test.go
+++ handlers/benefits_test.go
@@ -1,19 +1,20 @@
 package handlers
 
 import (
-	"github.com/DTS-STN/benefit-service/src/benefits"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/DTS-STN/benefit-service/src/benefits"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBenefits(t *testing.T) {
 	// Setup Echo service
 	e := echo.New()
 	//Load Benefits Data File
-	benefits.BenefitsService = benefits.BenefitsServiceStruct{Filename: "../benefit_info.json"}
+	benefits.BenefitsService = benefits.BenefitsServiceStruct{Filename: "../benefit_info_en.json"}
 	// Setup http request using httptest
 	req := httptest.NewRequest(http.MethodGet, "/benefits", nil)
 	// Create a httptest record
--- handlers/life_journey_test.go
+++ handlers/life_journey_test.go
@@ -1,19 +1,20 @@
 package handlers
 
 import (
-	"github.com/DTS-STN/benefit-service/src/lifejourneys"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/DTS-STN/benefit-service/src/lifejourneys"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLifeJourney(t *testing.T) {
 	// Setup Echo service
 	e := echo.New()
 
-	lifejourneys.LifeJourneyService = lifejourneys.LifeJourneyServiceStruct{Filename: "../life_journeys.json"}
+	lifejourneys.LifeJourneyService = lifejourneys.LifeJourneyServiceStruct{Filename: "../life_journeys_en.json"}
 	// Setup http request using httptest
 	req := httptest.NewRequest(http.MethodGet, "/lifejourney", nil)
 	// Create a httptest record
