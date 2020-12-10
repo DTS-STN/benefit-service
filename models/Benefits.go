0585af4415dfcf64b5a5a13f9091f0608547af15
MikeBungay <mikebungay76@gmail.com>
2020-12-10 11:32:07 -0500

    Added Long description to the root benefit data with Markdown description of the benefit details

--- models/Benefits.go
+++ models/Benefits.go
@@ -4,6 +4,7 @@ type Benefits struct {
 	ID              string         `json:"id"`
 	Title           string         `json:"title"`
 	Description     string         `json:"description"`
+	LongDescription string         `json:"long_description"`
 	RelatedBenefits []string       `json:"related_benefits"`
 	BenefitDetails  []FieldDetails `json:"benefit_details"`
 }
