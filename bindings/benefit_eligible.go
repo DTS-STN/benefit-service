f551ace9eb8e5b37560295af2bdbfd0a3aa1f3bc
krisc <char0799@algonquinlive.com>
2021-02-08 14:56:13 -0500

    Added province field to eligibility request struct

--- bindings/benefit_eligible.go
+++ bindings/benefit_eligible.go
@@ -6,4 +6,5 @@ type BenefitEligibilityRequest struct {
 	AbleToWork          string `json:"ableToWork"`
 	ReasonForSeparation string `json:"reasonForSeparation"`
 	Gender              string `json:"gender"`
+	Province            string `json:"province"`
 }
